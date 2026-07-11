0aa2b68e80d076e6ffd3935f030c172a271454ae
Usman Ghani <usman.dev24@gmail.com>
2026-07-11 18:20:45 +0500

    feat/refractor(auth-svc/drizzle): refractor authsvc database ORM from… (#4)
    * feat/refractor(auth-svc/drizzle): refractor authsvc database ORM from prisma to drizzle-orm, app.mjs refractor, users-db.mjs refractor, docker compose files updated
    
    - In production database apply drizzle migration manually  as bun run drizzlekit-prod pull -init
    
    * fix/chore(auth-svc/dockerfile): fixed docker file release section issues
    
    * fix/docker(post-svc): After adding tests and seprating app.mjs and server.mjs, dockerfile CMD updated
    
    - I forgot to do this because on railway still old dockerfile is running.
    
    * chore(docker): dockerfile update, update compose.prod.yaml to match the dockerfiles
    
    * chore(auth-svc): add warning durring migrating  to drizzle

--- auth-svc/tests/api.test.mjs
+++ auth-svc/tests/api.test.mjs
@@ -9,99 +9,98 @@ const token = await sign(
   "HS256",
 );
 
-test("should pass to /list", async () => {
-  const res = await app.request("/list", {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+for (let loop = 0; loop < 1; loop++) {
+  test("should pass to /list", async () => {
+    const res = await app.request("/list", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    expect(res.status).toBe(200);
   });
-  ;
-  expect(res.status).toBe(200);
-});
 
-test("should pass to /create-user", async () => {
-  const passhash = await await bcrypt.hash("test", 10)
-  const res = await app.request("/create-user", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-    
-    body: JSON.stringify({
-      username: "testUser",
-      password_hash: passhash,
-      fullName: "Test User",
-      firstName: "Test",
-      lastName: "User",
-      email: "test@test.com",
-    }),
-  });
-  
-  expect(res.status).toBe(200);
-});
-test("should pass to /find-or-create find first user", async () => {
-  const passhash = await await bcrypt.hash("test", 10)
-  const res = await app.request("/find-or-create", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-    
-    body: JSON.stringify({
-      username: "testUser",
-      password_hash: passhash,
-      fullName: "Test User",
-      firstName: "Test",
-      lastName: "User",
-      email: "test@test.com",
-    }),
-  });
-  
-  expect(res.status).toBe(200);
-});
+  test("should pass to /create-user", async () => {
+    const passhash = await await bcrypt.hash("test", 10);
+    const res = await app.request("/create-user", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
 
-test("should pass to /create-user 2nd user", async () => {
-  const passhash = await await bcrypt.hash("test", 10)
-  const res = await app.request("/create-user", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-    
-    body: JSON.stringify({
-      username: "testuser2",
-      password_hash: passhash,
-      fullName: "Test User 2",
-      firstName: "Test2",
-      lastName: "User2",
-      email: "test2@test.com",
-    }),
-  });
-  expect(res.status).toBe(200);
-});
+      body: JSON.stringify({
+        username: "testUser",
+        password_hash: passhash,
+        fullName: "Test User",
+        firstName: "Test",
+        lastName: "User",
+        email: "test@test.com",
+      }),
+    });
 
-test("should pass to /destroy", async () => {
-  const res = await app.request("/destroy/testUser", {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+    expect(res.status).toBe(200);
   });
-  ;
-  expect(res.status).toBe(200);
-});
+  test("should pass to /find-or-create find first user", async () => {
+    const passhash = await await bcrypt.hash("test", 10);
+    const res = await app.request("/find-or-create", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
 
-test("should pass to /destroy 2nd user", async () => {
-  const res = await app.request("/destroy/testuser2", {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+      body: JSON.stringify({
+        username: "testUser",
+        password_hash: passhash,
+        fullName: "Test User",
+        firstName: "Test",
+        lastName: "User",
+        email: "test@test.com",
+      }),
+    });
+
+    expect(res.status).toBe(200);
   });
-  ;
-  expect(res.status).toBe(200);
-});
+
+  test("should pass to /create-user 2nd user", async () => {
+    const passhash = await await bcrypt.hash("test", 10);
+    const res = await app.request("/create-user", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        username: "testuser2",
+        password_hash: passhash,
+        fullName: "Test User 2",
+        firstName: "Test2",
+        lastName: "User2",
+        email: "test2@test.com",
+      }),
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test("should pass to /destroy", async () => {
+    const res = await app.request("/destroy/testUser", {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test("should pass to /destroy 2nd user", async () => {
+    const res = await app.request("/destroy/testuser2", {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    expect(res.status).toBe(200);
+  });
+}
